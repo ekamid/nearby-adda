b21e96139e7fda678e636bade93fa57c00cbb6b5
ekamid <ebrahimkhalilamidbd@gmail.com>
2023-02-28 03:14:44 +0600

    experimenting with varcel

--- routes/api.js
+++ routes/api.js
@@ -5,45 +5,52 @@ const userRouter = express.Router();
 
 const auth = require("../middlewares/auth");
 const AuthController = require("../controllers/AuthController");
-const PostController = require("../controllers/PostController");
-const CategoryController = require("../controllers/CategoryController");
-const AlbumController = require("../controllers/AlbumController");
+// const PostController = require("../controllers/PostController");
+// const CategoryController = require("../controllers/CategoryController");
+// const AlbumController = require("../controllers/AlbumController");
 
-const {
-  registerValidationRules,
-  registerValidate,
-  loginValidationRules,
-  loginValidate,
-} = require("../middlewares/validators");
+// const {
+//   registerValidationRules,
+//   registerValidate,
+//   loginValidationRules,
+//   loginValidate,
+// } = require("../middlewares/validators");
 
-router.post(
-  "/register",
-  registerValidationRules(),
-  registerValidate,
-  AuthController.register
-);
+router.get("/", async (req, res, next) => {
+  return res.status(200).json({
+    title: "Express Testing",
+    message: "The app is working properly!",
+  });
+});
 
-router.post(
-  "/login",
-  loginValidationRules(),
-  loginValidate,
-  AuthController.login
-);
+// router.post(
+//   "/register",
+//   registerValidationRules(),
+//   registerValidate,
+//   AuthController.register
+// );
 
-//grouping routes
-userRouter.use(auth);
+// router.post(
+//   "/login",
+//   loginValidationRules(),
+//   loginValidate,
+//   AuthController.login
+// );
 
-// storing data
-router.post("/category/store", auth, CategoryController.storeCategory);
-router.post("/album/store", auth, PostController.storeAlbum);
-router.post("/post/store", auth, AlbumController.storePost);
+// //grouping routes
+// userRouter.use(auth);
 
-//fetching data
-router.get("/posts/:id", auth, PostController.getPostsByUserId);
-router.get("/post/:id", auth, PostController.getPostById);
-router.get("/categories/:id", auth, CategoryController.getCategoriesByUserId);
-router.get("/category/:id", auth, CategoryController.getCategoryById);
-router.get("/albums/:id", auth, AlbumController.getAlbumsByUserId);
-router.get("/album/:id", auth, AlbumController.getAlbumById);
+// // storing data
+// router.post("/category/store", auth, CategoryController.storeCategory);
+// router.post("/album/store", auth, PostController.storeAlbum);
+// router.post("/post/store", auth, AlbumController.storePost);
+
+// //fetching data
+// router.get("/posts/:id", auth, PostController.getPostsByUserId);
+// router.get("/post/:id", auth, PostController.getPostById);
+// router.get("/categories/:id", auth, CategoryController.getCategoriesByUserId);
+// router.get("/category/:id", auth, CategoryController.getCategoryById);
+// router.get("/albums/:id", auth, AlbumController.getAlbumsByUserId);
+// router.get("/album/:id", auth, AlbumController.getAlbumById);
 
 module.exports = router;
