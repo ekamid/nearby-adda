79f354ede21fe2c65bfcb0915bd0af622d77370b
ekamid <ebrahimkhalilamidbd@gmail.com>
2023-03-07 01:33:41 +0600

    update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,11 @@
 module.exports = {
+  extends: ["prettier", "eslint:recommended"],
   env: {
     node: true,
     commonjs: true,
     es2021: true,
     mocha: true,
   },
-  extends: "eslint:recommended",
   overrides: [],
   parserOptions: {
     ecmaVersion: "latest",
