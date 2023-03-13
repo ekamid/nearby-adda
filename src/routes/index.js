043537d3771255a1faf83cd2a65bc0640bae0623
ekamid <ebrahimkhalilamidbd@gmail.com>
2023-03-13 20:05:25 +0600

    index api message update

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,8 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
   return res.json({
-    message: "Hello to the Nearby Adda API",
+    message:
+      "Hello to the Nearby Adda API! Visit https://nearby-adda-api.vercel.app/v1/api-docs/ to see API documentation!",
   });
 });
 
